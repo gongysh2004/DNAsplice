8191a53f8b2e2edb4f9f2bb058074ea2e41320bc
Larry Liao <68320107+larryliao28@users.noreply.github.com>
2022-03-21 19:40:07 -0500

    Add files via upload

---
+++ mp-dna-splicing-larryliao28/includes/dna_strand.hpp
@@ -0,0 +1,21 @@
+#ifndef DNA_STRAND_HPP
+#define DNA_STRAND_HPP
+
+#include "node.hpp"
+
+class DNAstrand {
+public:
+  DNAstrand() = default;
+  DNAstrand(const DNAstrand& rhs) = delete;
+  DNAstrand& operator=(const DNAstrand& rhs) = delete;
+  ~DNAstrand();
+
+  void SpliceIn(const char* pattern, DNAstrand& to_splice_in);
+  int StrLen(const char* c_str);
+
+private:
+  Node* head_ = nullptr;
+  Node* tail_ = nullptr;
+};
+
+#endif
